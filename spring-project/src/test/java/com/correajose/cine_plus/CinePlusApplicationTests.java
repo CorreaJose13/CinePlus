d3a6480aae0c6abc60184767fd02c4bff30edc5c
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-02-27 13:03:34 -0500

    Create spring project

---
+++ spring-project/src/test/java/com/correajose/cine_plus/CinePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correajose.cine_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
