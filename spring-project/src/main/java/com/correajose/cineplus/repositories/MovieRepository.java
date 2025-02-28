c33c7c8b64c9c78bd0dee325018167f8c148b563
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-02-28 18:11:28 -0500

    add existsByReleaseDateAndTitle query

--- spring-project/src/main/java/com/correajose/cineplus/repositories/MovieRepository.java
+++ spring-project/src/main/java/com/correajose/cineplus/repositories/MovieRepository.java
@@ -3,8 +3,10 @@ package com.correajose.cineplus.repositories;
 import com.correajose.cineplus.models.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
 import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie,Integer> {
     Optional<Movie> findByTitle(String title);
+    boolean existsByReleaseDateAndTitle(Date releaseDate, String title);
 }
