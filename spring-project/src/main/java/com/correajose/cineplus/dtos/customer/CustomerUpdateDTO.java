452814962f8649304ee8616d899fdbcd88a94bd9
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-02-28 07:47:32 -0500

    add customer dtos

---
+++ spring-project/src/main/java/com/correajose/cineplus/dtos/customer/CustomerUpdateDTO.java
@@ -0,0 +1,21 @@
+package com.correajose.cineplus.dtos.customer;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerUpdateDTO {
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Last name cannot be empty")
+    private String lastName;
+
+    @NotBlank(message = "Phone cannot be empty")
+    /* Añadir patron telefono*/
+    private String phone;
+}
