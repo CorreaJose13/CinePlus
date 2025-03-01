014854d6ac75e25d2cb410c292ba75a23d272297
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-03-01 14:20:28 -0500

    implement movie function endpoint

---
+++ spring-project/src/main/java/com/correajose/cineplus/repositories/MovieFunctionRepository.java
@@ -0,0 +1,7 @@
+package com.correajose.cineplus.repositories;
+
+import com.correajose.cineplus.models.movie.MovieFunction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieFunctionRepository extends JpaRepository<MovieFunction, Integer> {
+}
