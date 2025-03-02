171dfbddb4947f6107e777ca207f0cc3bbac0e96
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-03-02 13:38:18 -0500

    lang from JS to TS and add vuetify

---
+++ front-end/src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" app dark>
+      <div class="d-flex align-center">
+        <v-img alt="Logo" class="mr-2" contain src="@/assets/logo.svg" width="40" height="40" />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="navigation-links">
+        <v-btn v-for="item in navigationItems" :key="item.title" :to="item.to" text class="mx-2">
+          {{ item.title }}
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <RouterView />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      navigationItems: [
+        { title: 'Cartelera', to: '/cartelera' },
+        { title: 'Iniciar Sesión', to: '/login' },
+        { title: 'Registrarse', to: '/register' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navigation-links {
+  display: flex;
+  align-items: center;
+}
+
+@media (max-width: 600px) {
+  .navigation-links {
+    display: none;
+  }
+}
+</style>
---
+++ front-end/src/plugins/vuetify.ts
@@ -0,0 +1,38 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#01793E', // green
+          secondary: '#CE1227', // red
+        },
+      },
+    },
+  },
+})
