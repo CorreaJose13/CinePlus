137f2987b9c8d8d3d7fe4b11b57531ee8b5d6fdd
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-03-03 19:51:33 -0500

    Update nav bar

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -2,7 +2,7 @@
 import { ref, computed } from 'vue'
 import { useDisplay } from 'vuetify'
 const navigationItems = ref<{ title: string; to: string; icon?: string }[]>([
-  { title: 'Cartelera', to: '/billboard', icon: 'mdi-movie' },
+  { title: 'Cartelera', to: '/', icon: 'mdi-movie' },
   { title: 'Iniciar Sesión', to: '/login', icon: 'mdi-login' },
   { title: 'Registrarse', to: '/register', icon: 'mdi-account-plus' },
 ])
@@ -13,38 +13,40 @@ const isMobile = computed(() => mobile.value)
 </script>
 
 <template>
-  <v-app-bar color="primary" :elevation="10">
-    <v-spacer></v-spacer>
+  <v-app-bar color="primary" :elevation="10" density="prominent" class="app-bar">
+    <v-container class="d-flex align-self-center">
+      <v-spacer></v-spacer>
 
-    <div v-if="!isMobile" class="navigation-links">
-      <v-btn
-        v-for="item in navigationItems"
-        :key="item.title"
-        :to="item.to"
-        variant="text"
-        :prepend-icon="item.icon"
-        rounded
-        class="ms-1"
-      >
-        {{ item.title }}
-      </v-btn>
-    </div>
+      <div v-if="!isMobile" class="navigation-links">
+        <v-btn
+          v-for="item in navigationItems"
+          :key="item.title"
+          :to="item.to"
+          variant="text"
+          :prepend-icon="item.icon"
+          rounded
+          class="ms-1"
+        >
+          {{ item.title }}
+        </v-btn>
+      </div>
 
-    <v-app-bar-nav-icon v-if="isMobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon v-if="isMobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-container>
   </v-app-bar>
 
   <v-navigation-drawer v-model="drawer" temporary location="left">
-      <v-btn
-        v-for="item in navigationItems"
-        :key="item.title"
-        :to="item.to"
-        variant="text"
-        :prepend-icon="item.icon"
-        rounded
-        class="ms-1"
-      >
-        {{ item.title }}
-      </v-btn>
+    <v-btn
+      v-for="item in navigationItems"
+      :key="item.title"
+      :to="item.to"
+      variant="text"
+      :prepend-icon="item.icon"
+      rounded
+      class="ms-1"
+    >
+      {{ item.title }}
+    </v-btn>
   </v-navigation-drawer>
 </template>
 
