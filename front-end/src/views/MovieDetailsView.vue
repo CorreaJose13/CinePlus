dcb4ae4bed5cad86886fbab1ad512046fe77be03
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-03-03 19:27:30 -0500

    implement movie details view

---
+++ front-end/src/views/MovieDetailsView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import MovieInfo from '@/components/MovieInfo.vue'
+</script>
+
+<template>
+  <v-container>
+    <v-row no-gutters justify="center" class="d-flex">
+      <v-col>
+        <v-sheet class="pa-2 ma-2"> <MovieInfo /> </v-sheet>
+      </v-col>
+      <v-col cols="8">
+        <v-sheet class="pa-2 ma-2">
+          <router-view></router-view>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
